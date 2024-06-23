36ec6b647a81616b8e2b4b3f73589f41c5882fa6
rasaq-ola <olatech@DESKTOP-2QK9NGF>
2024-06-23 15:39:57 +0100

    sorting algorithm

--- sort.h
+++ sort.h
@@ -1,37 +1,27 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stddef.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
  * struct listint_s - Doubly linked list node
- *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
-    int n;
+    int n; /* Removed the 'const' qualifier */
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
 
-/* Print functions */
-void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-/* Bubble Sort */
+void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
-
-/* Insertion Sort */
 void insertion_sort_list(listint_t **list);
-
-/* Selection Sort */
 void selection_sort(int *array, size_t size);
-
-/* Quick Sort */
 void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
