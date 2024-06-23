d53aa0c1152ca5fb7baac4a0597e1554c6c0f7ca
rasaq-ola <olatech@DESKTOP-2QK9NGF>
2024-06-23 16:07:25 +0100

    sorting algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,54 +3,37 @@
 #include "sort.h"
 
 /**
- * create_node - Creates a new node
- * @n: Value to store in the new node
- * Return: Address of the new node, or NULL on failure
- */
-listint_t *create_node(const int n)
-{
-	listint_t *node;
-
-	node = malloc(sizeof(listint_t));
-	if (node == NULL)
-		return (NULL);
-
-	node->n = n;
-	node->prev = NULL;
-	node->next = NULL;
-
-	return (node);
-}
-
-/**
  * insertion_sort_list - Sorts a doubly linked list of integers in ascd. order
  * @list: Double pointer to the head of the linked list
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *temp, *current;
+	listint_t *current, *next_node, *temp;
 
-	if (!list || !*list || !(*list)->next)
+	if (!list || !*list)
 		return;
 
-	temp = *list;
-	while (temp->next)
+	current = (*list)->next;
+	while (current)
 	{
-		current = temp->next;
-		while (current && current->prev && current->n < current->prev->n)
+		next_node = current->next;
+		while (current->prev && current->n < current->prev->n)
 		{
+			temp = current->prev;
+			/* Swap nodes */
 			if (current->next)
-				current->next->prev = current->prev;
-			current->prev->next = current->next;
-			current->next = current->prev;
-			current->prev = current->prev->prev;
-			current->next->prev = current;
-			if (current->prev == NULL)
-				*list = current;
-			else
+				current->next->prev = temp;
+			temp->next = current->next;
+			current->next = temp;
+			current->prev = temp->prev;
+			temp->prev = current;
+			if (current->prev)
 				current->prev->next = current;
+			else
+				*list = current;
+
 			print_list(*list);
 		}
-		temp = temp->next;
+		current = next_node;
 	}
 }
